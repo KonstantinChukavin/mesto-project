663e92c9c2bceda05d05b811be6634052150af2d
KonstantinChukavin <KainkosYa@yandex.ru>
2022-03-16 10:41:33 +0300

    feature popup
    Начало верстки попапов по фигме

---
+++ script/script.js
@@ -0,0 +1,27 @@
+// загружаемые фото
+const initialCards = [
+    {
+        name: 'Амстердам',
+        link: './images/elements/Амстердам.jpg'
+    },
+    {
+        name: 'Афины',
+        link: './images/elements/Афины2.jpg'
+    },
+    {
+        name: 'ОАЭ',
+        link: './images/elements/ОАЭ.jpg'
+    },
+    {
+        name: 'Флоренция',
+        link: './images/elements/Флоренция.jpg'
+    },
+    {
+        name: 'Турция',
+        link: './images/elements/Турция.jpg'
+    },
+    {
+        name: 'Вена',
+        link: './images/elements/Вена.jpg'
+    }
+];
